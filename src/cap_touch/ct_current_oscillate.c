1924dab13e7bcbc0b380140adbabc02d4769d94b
ReinBentdal <rein.bent@gmail.com>
2024-09-12 20:05:35 +0200

    funker på MONKEY! Isource osc + counter

--- src/cap_touch/ct_current_oscillate.c
+++ src/cap_touch/ct_current_oscillate.c
@@ -30,11 +30,11 @@ static void _configure_counter(void);
 static void _configure_rtc(void);
 static void _configure_ppi(void);
 
-static void _comp_isr(void);
 static void _rtc_isr(void);
 
-static int _timer_clear_ppi_idx;
-static int _timer_capture_ppi_idx;
+static int _counter_count_ppi_idx;
+static int _counter_clear_ppi_idx;
+static int _counter_capture_ppi_idx;
 
 void cap_touch_init(void) {
     LOG_INF("cap_touch_init");
@@ -42,24 +42,26 @@ void cap_touch_init(void) {
     const struct hardware_spec* hw_spec = hardware_spec_get();
     RETURN_ON_ERR_MSG(hw_spec->cap_touch_psel == SAADC_CH_PSELP_PSELP_NC, "no cap touch analog pin");
 
-    uint32_t cap_psel = hardware_spec_pin_get(HARDWARE_SPEC_PIN_CAPTOUCH);
-    RETURN_ON_ERR_MSG(cap_psel < 0, "no cap touch pin");
+    _counter_count_ppi_idx = ppi_index_get();
+    _counter_clear_ppi_idx = ppi_index_get();
+    _counter_capture_ppi_idx = ppi_index_get();
 
-    // _timer_clear_ppi_idx = ppi_index_get();
-    // _timer_capture_ppi_idx = ppi_index_get();
-
-    _configure_comparator(cap_psel);
+    _configure_comparator(hw_spec->cap_touch_psel);
+    _configure_counter();
+    _configure_rtc();
+    _configure_ppi();
 
     LOG_INF("cap_touch_init done");
 }
 
 void cap_touch_start(void) {
-    // NRF_TIMER0->TASKS_START = 1;
-    // NRF_TIMER0->TASKS_CLEAR = 1;
+    LOG_INF("cap_touch_start");
+
+    NRF_TIMER0->TASKS_CLEAR = 1;
+    NRF_TIMER0->TASKS_START = 1;
 
     NRF_COMP->TASKS_START = 1;
-
-    // NRF_RTC0->TASKS_START = 1;
+    NRF_RTC0->TASKS_START = 1;
 }
 
 static void _configure_comparator(int psel) {
@@ -79,16 +81,10 @@ static void _configure_comparator(int psel) {
     NRF_COMP->ISOURCE = COMP_ISOURCE_ISOURCE_Ien10mA << COMP_ISOURCE_ISOURCE_Pos;
 
     // set pin
-    NRF_COMP->PSEL = COMP_PSEL_PSEL_AnalogInput1;
-
-    // set COMP to trigger on DOWN event
-    NRF_COMP->INTENSET = COMP_INTEN_DOWN_Enabled << COMP_INTEN_DOWN_Pos;
+    NRF_COMP->PSEL = psel;
     
     // enable and start
     NRF_COMP->ENABLE = COMP_ENABLE_ENABLE_Enabled << COMP_ENABLE_ENABLE_Pos;
-
-    IRQ_CONNECT(COMP_LPCOMP_IRQn, 3, _comp_isr, 0, 0);
-    irq_enable(COMP_LPCOMP_IRQn);
 }
 
 static void _configure_counter(void) {
@@ -99,50 +95,51 @@ static void _configure_counter(void) {
 
     // use compare to trigger a cap touch press event. The RTC should normally reset the timer before this happens
     // NRF_TIMER0->CC[0] = 0xFFFF;
-
     // NRF_TIMER0->INTENSET = TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos;
 }
 
+// use RTC to trigger counter reset on CC using PPI
 static void _configure_rtc(void) {
-    // use RTC to trigger counter reset on CC using PPI
+    // alternatively, if its a problem clearing the counter at the same time as capture, use CC0 and CC1 just after each other to do them in sequence
 
     // some generic values for now
-    NRF_RTC0->PRESCALER = 0;
-    NRF_RTC0->CC[0] = 1000;
+    NRF_RTC0->PRESCALER = 327; // f = 100Hz
 
-    NRF_RTC0->EVTENSET = RTC_EVTEN_COMPARE0_Enabled << RTC_EVTEN_COMPARE0_Pos;
-    
-    // configure interrupt to log value
-    NRF_RTC0->INTENSET = RTC_INTENSET_COMPARE0_Enabled << RTC_INTENSET_COMPARE0_Pos;
+    NRF_RTC0->EVTENSET = RTC_EVTEN_TICK_Enabled << RTC_EVTEN_TICK_Pos;
+    NRF_RTC0->INTENSET = RTC_INTENSET_TICK_Enabled<< RTC_INTENSET_TICK_Pos; // configure interrupt to log value
 
     IRQ_CONNECT(RTC0_IRQn, 3, _rtc_isr, 0, 0);
+    irq_enable(RTC0_IRQn);
 }
 
 static void _configure_ppi(void) {
     // its possible to add them all to a group to be able to enable and disable them all at once using task
 
+    // connect COMP to TIMER0: count
+    NRF_PPI->CH[_counter_count_ppi_idx].EEP = (uint32_t)&NRF_COMP->EVENTS_DOWN;
+    NRF_PPI->CH[_counter_count_ppi_idx].TEP = (uint32_t)&NRF_TIMER0->TASKS_COUNT;
+    NRF_PPI->CHENSET = 1 << _counter_count_ppi_idx;
+
     // connect COMP to TIMER0: capture timer counter
     // used by interrupt to log the counter value
-    NRF_PPI->CH[_timer_capture_ppi_idx].EEP = (uint32_t)&NRF_RTC0->EVENTS_COMPARE[0];
-    NRF_PPI->CH[_timer_capture_ppi_idx].TEP = (uint32_t)&NRF_TIMER0->TASKS_CAPTURE[1];
-    NRF_PPI->CHENSET = 1 << _timer_capture_ppi_idx;
+    NRF_PPI->CH[_counter_capture_ppi_idx].EEP = (uint32_t)&NRF_RTC0->EVENTS_TICK;
+    NRF_PPI->CH[_counter_capture_ppi_idx].TEP = (uint32_t)&NRF_TIMER0->TASKS_CAPTURE[1];
+    NRF_PPI->CHENSET = 1 << _counter_capture_ppi_idx;
 
     // connect RTC to TIMER0: reset timer counter
-    NRF_PPI->CH[_timer_clear_ppi_idx].EEP = (uint32_t)&NRF_RTC0->EVENTS_COMPARE[0];
-    NRF_PPI->CH[_timer_clear_ppi_idx].TEP = (uint32_t)&NRF_TIMER0->TASKS_CLEAR;
-    NRF_PPI->CHENSET = 1 << _timer_clear_ppi_idx;
+    NRF_PPI->CH[_counter_clear_ppi_idx].EEP = (uint32_t)&NRF_RTC0->EVENTS_TICK;
+    NRF_PPI->CH[_counter_clear_ppi_idx].TEP = (uint32_t)&NRF_TIMER0->TASKS_CLEAR;
+    NRF_PPI->CHENSET = 1 << _counter_clear_ppi_idx;
 }
 
 static void _cap_event(struct k_work* work) {
     LOG_INF("_cap_event");
 }
 
-static void _comp_isr(void) {
-    if (NRF_COMP->EVENTS_DOWN) {
-        NRF_COMP->EVENTS_DOWN = 0;
-        k_work_submit(&_cap_touch_work);
-        // LOG_INF("DOWN");
+static void _rtc_isr(void) {
+    if (NRF_RTC0->EVENTS_TICK) {
+        NRF_RTC0->EVENTS_TICK = 0;
+        // LOG_INF("Counter read: %d", NRF_TIMER0->CC[1]);
+        printk("%d\n", NRF_TIMER0->CC[1]);
     }
 }
-
-static void _rtc_isr(void);
